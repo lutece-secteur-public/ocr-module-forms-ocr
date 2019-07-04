0eb250e14dfaa334f9663ce2cfcf7457589af571
Unknown <geoffroy.huet@accenture.com>
2019-07-04 11:52:01 +0200

    MFORMSOCR-9: Add error message in properties

--- src/java/fr/paris/lutece/plugins/genericattributes/modules/ocr/utils/OcrProviderUtils.java
+++ src/java/fr/paris/lutece/plugins/genericattributes/modules/ocr/utils/OcrProviderUtils.java
@@ -24,6 +24,7 @@ import fr.paris.lutece.plugins.genericattributes.modules.ocr.business.Mapping;
 import fr.paris.lutece.plugins.genericattributes.modules.ocr.business.MappingHome;
 import fr.paris.lutece.plugins.genericattributes.modules.ocr.exceptions.CallOcrException;
 import fr.paris.lutece.portal.service.editor.EditorBbcodeService;
+import fr.paris.lutece.portal.service.i18n.I18nService;
 import fr.paris.lutece.portal.service.template.AppTemplateService;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
@@ -45,6 +46,9 @@ public class OcrProviderUtils {
     public static final String ENTRY_TYPE_RADIOBUTTON = AppPropertiesService.getProperty( "genericattributes-ocr.entry.radiobutton" );
     public static final String ENTRY_TYPE_SELECT = AppPropertiesService.getProperty( "genericattributes-ocr.entry.select" );
     public static final String ENTRY_TYPE_TEXTAREA = AppPropertiesService.getProperty( "genericattributes-ocr.entry.textarea" );
+    
+    //Error message
+    private static String MESSAGE_ERROR_OCR = "module.genericattributes.ocr.error.callOcr";
 	
 	public static HtmlTemplate builtTempalteConfiog ( ReferenceList listEntry, IOcrProvider ocrProvider, int nIdTargetEntry, String strResourceType ){
 		
@@ -109,7 +113,7 @@ public class OcrProviderUtils {
 
         } catch (  IOException | HttpAccessException | IllegalArgumentException e)
         {
-        	throw new CallOcrException("Erreur lors de l'appel de l'OCR", e);
+        	throw new CallOcrException(I18nService.getLocalizedString( MESSAGE_ERROR_OCR, Locale.getDefault() ), e);
         }
     }
 	
