31b443595f1c29896d5c3e32f79b4261a0f582aa
Guillaume Lépine <guillaume.lepine@sogeti.com>
2019-07-22 14:57:27 +0200

    MFORMSOCR-11 : Processing create event (called when a copy occurs)

--- src/java/fr/paris/lutece/plugins/genericattributes/modules/ocr/service/listener/OcrEntryListener.java
+++ src/java/fr/paris/lutece/plugins/genericattributes/modules/ocr/service/listener/OcrEntryListener.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import fr.paris.lutece.plugins.genericattributes.modules.ocr.business.Mapping;
 import fr.paris.lutece.plugins.genericattributes.modules.ocr.business.MappingHome;
+import fr.paris.lutece.plugins.genericattributes.util.CopyEntryEventParam;
 import fr.paris.lutece.portal.business.event.EventRessourceListener;
 import fr.paris.lutece.portal.business.event.ResourceEvent;
 
@@ -13,8 +14,23 @@ public class OcrEntryListener implements EventRessourceListener {
 	public static final String BEAN_NAME = "genericattributes-ocr.ocrEntryListener";
 	 
 	@Override
-	public void addedResource( ResourceEvent arg0 )
+	public void addedResource( ResourceEvent event )
 	{
+		Integer oldId = null;
+		if ( event.getParam( ) != null && event.getParam( ) instanceof CopyEntryEventParam )
+		{
+			oldId = ( (CopyEntryEventParam) event.getParam( ) ).getValue( );
+		}
+		
+		if ( oldId != null )
+		{
+			List<Mapping> mappingList = MappingHome.loadMappingByTargetEntry( oldId, event.getTypeResource( ) );
+			for( Mapping mapping : mappingList )
+			{
+				mapping.setIdTargetEntry( Integer.valueOf( event.getIdResource( ) ) );
+				MappingHome.create( mapping );
+			}
+		}
 	}
 
 	@Override
